6e27e7b20b585499b87532d844be8d6af91b541c
Zachary <zbberman@yahoo.com>
2019-02-08 18:17:00 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,26 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  #create a hash of each letter with a numerical value that you can rank by 
+  alphabet_hash = {}
+  alphabet.chars.each_with_index do | letter, index |
+    alphabet_hash[letter] = index + 1 
+  end 
+  alphabet_hash[" "] = 0 
+  
+  #create a hash and assign an array of values for each word 
+  arr_hash = {}
+  
+  arr.each do | word | 
+    arr_hash[word] = []
+    word.chars.each do | letter |
+      arr_hash[word] << alphabet_hash[letter]
+    end 
+  end 
+  
+  #sort by values added to array
+  arr.sort_by do | word | 
+    arr_hash[word]
+  end 
+  
 end
